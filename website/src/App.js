2d3d88f8a84be9e156f7615e4ff220fafe32f26d
Kavya Ravikanti <Kavya@Kavyas-MacBook-Pro.local>
2017-06-29 22:58:19 -0400

    main page with basic transition

---
+++ website/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+import sittingme from "./images/sittingme.jpg";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="main-overlay">
+          <div className="name"> KAVYA RAVIKANTI </div>
+          <div className="desc"> Software Developer</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
