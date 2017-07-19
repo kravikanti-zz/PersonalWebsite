a55f66cbe99785c8f25f8ac859fd4a38a56f00b7
Kavya Ravikanti <kravikanti@users.noreply.github.com>
2017-07-19 00:51:14 -0400

    Add timeline

--- website/src/App.js
+++ website/src/App.js
@@ -152,6 +152,16 @@ class App extends Component {
                   </svg>
                   <span>React.js</span>
                 </li>
+                <li className="dev-skill">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+                    <g fill="#ffffff">
+                      <path d="M65.6 65.4c2.9-.3 5.1-2.8 5-5.8-.1-3-2.6-5.4-5.6-5.4h-.2c-3.1.1-5.5 2.7-5.4 5.8.1 1.5.7 2.8 1.6 3.7-3.4 6.7-8.6 11.6-16.4 15.7-5.3 2.8-10.8 3.8-16.3 3.1-4.5-.6-8-2.6-10.2-5.9-3.2-4.9-3.5-10.2-.8-15.5 1.9-3.8 4.9-6.6 6.8-8-.4-1.3-1-3.5-1.3-5.1-14.5 10.5-13 24.7-8.6 31.4 3.3 5 10 8.1 17.4 8.1 2 0 4-.2 6-.7 12.8-2.5 22.5-10.1 28-21.4z"/>
+                      <path d="M83.2 53c-7.6-8.9-18.8-13.8-31.6-13.8H50c-.9-1.8-2.8-3-4.9-3h-.2c-3.1.1-5.5 2.7-5.4 5.8.1 3 2.6 5.4 5.6 5.4h.2c2.2-.1 4.1-1.5 4.9-3.4H52c7.6 0 14.8 2.2 21.3 6.5 5 3.3 8.6 7.6 10.6 12.8 1.7 4.2 1.6 8.3-.2 11.8-2.8 5.3-7.5 8.2-13.7 8.2-4 0-7.8-1.2-9.8-2.1-1.1 1-3.1 2.6-4.5 3.6 4.3 2 8.7 3.1 12.9 3.1 9.6 0 16.7-5.3 19.4-10.6 2.9-5.8 2.7-15.8-4.8-24.3z"/>
+                      <path d="M32.4 67.1c.1 3 2.6 5.4 5.6 5.4h.2c3.1-.1 5.5-2.7 5.4-5.8-.1-3-2.6-5.4-5.6-5.4h-.2c-.2 0-.5 0-.7.1-4.1-6.8-5.8-14.2-5.2-22.2.4-6 2.4-11.2 5.9-15.5 2.9-3.7 8.5-5.5 12.3-5.6 10.6-.2 15.1 13 15.4 18.3 1.3.3 3.5 1 5 1.5-1.2-16.2-11.2-24.6-20.8-24.6-9 0-17.3 6.5-20.6 16.1-4.6 12.8-1.6 25.1 4 34.8-.5.7-.8 1.8-.7 2.9z"/>
+                    </g>
+                  </svg>
+                  <span>Redux</span>
+                </li>
               </ul>
             </div>
             <div className="skills-content">
@@ -186,6 +196,88 @@ class App extends Component {
           <div className="exp-heading">
             Experience
           </div>
+          <div id="experience-section" className="section gap">
+              {/*<div className="bg" data-anchor-target="#experience-section" data-bottom-top="transform: translate3d(0px, -150px, 0px);"
+                  data-top-bottom="transform: translate3d(0px, 150px, 0px);"></div>*/}
+              <div className="overlay-wrapper">
+                  <div className="fade-in">
+                      {/* <div className="center">
+                          <h1 className="section-heading">Experience</h1> <span className="divider center"></span>
+                          <p className="experience-description">Thanks to Northeastern's co-op program, I've had the privilege of completing two awesome six-month
+                              co-ops as a Creative Technologist at <a href="http://us.mullenlowe.com/" target="_blank">MullenLowe U.S.</a>                            and Software Engineer at <a href="https://starry.com/" target="_blank">Starry</a>. Starting in
+                              July 2017, I'll be joining the iTunes team at <a href="https://apple.com/" target="_blank">Apple</a>                            as a UI Engineer for my third and final co-op!</p>
+                      </div> */}
+                      <div className="experience-wrapper">
+                          <div id="timeline">
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">University of Virginia</h2>
+                                      <h3 className="company"><a href="http://www.ccis.northeastern.edu/program/information-science-bs/" target="_blank">College of Arts & Sciences</a></h3>
+                                      <p className="job-desc">Bachelor of Arts in Computer Science with a minor in Global Studies. <strong>Graduating in May 2020.</strong></p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite university"></div>
+                                  </div> <span className="date">2016 - Present</span> </div>
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">EDIT</h2>
+                                      <h3 className="company"><a href="http://www.ccis.northeastern.edu/" target="_blank">EDIT</a></h3>
+                                      <p className="job-desc">EDIT</p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite research"></div>
+                                  </div> <span className="date">EDIT</span> </div>
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">EDIT</h2>
+                                      <h3 className="company"><a href="http://us.mullenlowe.com/" target="_blank">MullenLowe U.S.</a></h3>
+                                      <p className="job-desc">EDIT</p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite industry"></div>
+                                  </div> <span className="date">EDIT</span> </div>
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">EDIT</h2>
+                                      <h3 className="company"><a href="http://www.ccis.northeastern.edu/" target="_blank">EDIT</a></h3>
+                                      <p className="job-desc">EDIT</p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite academic"></div>
+                                  </div> <span className="date">EDIT</span> </div>
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">EDIT</h2>
+                                      <h3 className="company"><a href="http://nuwit.ccs.neu.edu" target="_blank">EDIT</a></h3>
+                                      <p className="job-desc">EDIT</p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite university"></div>
+                                  </div> <span className="date">EDIT</span> </div>
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">EDIT</h2>
+                                      <h3 className="company"><a href="https://starry.com/" target="_blank">Starry</a></h3>
+                                      <p className="job-desc">EDIT</p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite industry"></div>
+                                  </div> <span className="date">EDIT</span> </div>
+                              <div className="timeline-block">
+                                  <div className="timeline-content">
+                                      <h2 className="position">EDIT</h2>
+                                      <h3 className="company"><a href="http://www.northeastern.edu/scout/" target="_blank">EDIT</a></h3>
+                                      <p
+                                          className="job-desc">EDIT</p>
+                                  </div>
+                                  <div className="timeline-img">
+                                      <div className="sprite university"></div>
+                                  </div> <span className="date">EDIT</span> </div>
+                          </div> <i className="down-arrow"></i> </div>
+                  </div>
+              </div>
+          </div>
+          <a className="block-btn" href="http://brittanychiang.com/resume.pdf" target="_blank"> <span className="text">View my full resume</span> <img className="icon" src="./images/download.svg"/> </a>
         </div>
       </div>
     );
